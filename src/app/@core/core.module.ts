b98499d823906905cada6938f4a054ffc5c1e697
招文桃 <gdouzwt@gmail.com>
2018-05-16 00:08:43 +0800

    initial

---
+++ src/app/@core/core.module.ts
@@ -0,0 +1,157 @@
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NbAuthModule, NbDummyAuthProvider, NbEmailPassAuthProvider } from '@nebular/auth';
+import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
+import { of as observableOf } from 'rxjs/observable/of';
+
+import { throwIfAlreadyLoaded } from './module-import-guard';
+import { DataModule } from './data/data.module';
+import { AnalyticsService } from './utils/analytics.service';
+
+const socialLinks = [
+  {
+    url: 'https://github.com/akveo/nebular',
+    target: '_blank',
+    icon: 'socicon-github',
+  },
+  {
+    url: 'https://www.facebook.com/akveo/',
+    target: '_blank',
+    icon: 'socicon-facebook',
+  },
+  {
+    url: 'https://twitter.com/akveo_inc',
+    target: '_blank',
+    icon: 'socicon-twitter',
+  },
+];
+
+const NB_CORE_PROVIDERS = [
+  ...DataModule.forRoot().providers,
+
+  ...NbAuthModule.forRoot({
+    providers: {
+
+      email: {
+        service: NbEmailPassAuthProvider,
+        config: {
+          baseEndpoint: 'https://gdougym.cn/api/v1',
+          login: {
+            alwaysFail: false,
+            rememberMe: true,
+            endpoint: '/auth/sign-in',
+            method: 'post',
+            redirect: {
+              success: '/',
+              failure: null,
+            },
+            defaultErrors: ['邮箱或密码错误'],
+            defaultMessages: ['You have been successfully logged in.'],
+          },
+          register: {
+            alwaysFail: false,
+            rememberMe: true,
+            endpoint: '/auth/sign-up',
+            method: 'post',
+            redirect: {
+              success: '/',
+              failure: null,
+            },
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['You have been successfully registered.'],
+          },
+          logout: {
+             alwaysFail: false,
+             endpoint: '/auth/sign-out',
+             method: 'post',
+             redirect: {
+              success: '/',
+              failure: null,
+            },
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['You have been successfully logged out.'],
+           },
+           requestPass: {
+             endpoint: '/auth/request-pass',
+             method: 'post',
+             redirect: {
+              success: '/',
+              failure: null,
+            },
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['Reset password instructions have been sent to your email.'],
+           },
+           resetPass: {
+             endpoint: '/auth/reset-pass',
+             method: 'post',
+             redirect: {
+              success: '/',
+              failure: null,
+            },
+            resetPasswordTokenKey: 'reset_password_token',
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['Your password has been successfully changed.'],
+           },
+           
+         }
+      },
+    },
+
+    forms: {
+      login: {
+        socialLinks: socialLinks,
+      },
+      register: {
+        socialLinks: socialLinks,
+      },
+    },
+
+  }).providers,
+
+  NbSecurityModule.forRoot({
+    accessControl: {
+      guest: {
+        view: '*',
+      },
+      user: {
+        parent: 'guest',
+        create: '*',
+        edit: '*',
+        remove: '*',
+      },
+    },
+  }).providers,
+  {
+    provide: NbRoleProvider,
+    useValue: {
+      getRole: () => {
+        return observableOf('guest'); // here you could provide any role based on any auth flow
+      },
+    },
+  },
+  AnalyticsService,
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    NbAuthModule,
+  ],
+  declarations: [],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders>{
+      ngModule: CoreModule,
+      providers: [
+        ...NB_CORE_PROVIDERS,
+      ],
+    };
+  }
+}
---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,150 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: '仪表盘',
+    icon: 'nb-bar-chart',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: '系统功能',
+    group: true,
+  },
+  {
+    title: '智能门锁子系统',
+    icon: 'nb-locked',
+    link: '/pages/ui-features',
+    children: [
+      {
+        title: '开锁提示',
+        link: '/pages/ui-features/buttons',
+      },
+      {
+        title: '远程开锁',
+        link: '/pages/ui-features/grid',
+      },
+      {
+        title: '开锁联动',
+        link: '/pages/ui-features/icons',
+      },
+      {
+        title: '自动撤防',
+        link: '/pages/ui-features/modals',
+      },
+    ],
+  },
+  {
+    title: '智能情景子系统',
+    icon: 'nb-home',
+    children: [
+      {
+        title: '记忆功能',
+        link: '/pages/forms/inputs',
+      },
+      {
+        title: '情景重定义',
+        link: '/pages/forms/layouts',
+      },
+    ],
+  },
+  {
+    title: '智能照明子系统',
+    icon: 'nb-sunny-circled',
+    children: [
+      {
+        title: '手动控制',
+        link: '/pages/components/tree',
+      }, {
+        title: '明暗调节',
+        link: '/pages/components/notifications',
+      },
+    ],
+  },
+  {
+    title: '智能语音子系统',
+    icon: 'nb-volume-high',
+    children: [
+      {
+        title: '语音控制',
+        link: '/pages/maps/gmaps',
+      },
+      {
+        title: '手机控制',
+        link: '/pages/maps/leaflet',
+      },
+      {
+        title: '全开全关',
+        link: '/pages/maps/bubble',
+      },
+      {
+        title: '情景记忆功能',
+        link: '/pages/maps/searchmap',
+      },
+    ],
+  },
+  {
+    title: '智能安防子系统',
+    icon: 'nb-notifications',
+    children: [
+      {
+        title: '红外人体探测器',
+        link: '/pages/charts/echarts',
+      },
+      {
+        title: '智能门窗磁',
+        link: '/pages/charts/chartjs',
+      },
+      {
+        title: '室内云台摄像头',
+        link: '/pages/charts/d3',
+      },
+    ],
+  },
+  {
+    title: '智能背景音乐子系统',
+    icon: 'nb-audio',
+    children: [
+      {
+        title: '语音操控',
+        link: '/pages/editors/tinymce',
+      },
+      {
+        title: '云音乐',
+        link: '/pages/editors/ckeditor',
+      },
+    ],
+  },
+  {
+    title: '智能空调子系统',
+    icon: 'nb-snowy-circled',
+    children: [
+      {
+        title: '温度智能感应控制（恒温）',
+        link: '/pages/tables/smart-table',
+      },
+    ],
+  },
+  {
+    title: '智能插座子系统',
+    icon: 'nb-layout-two-column',
+    children: [
+      {
+        title: '远程通断电',
+        link: '/auth/login',
+      },
+      {
+        title: '儿童锁功能',
+        link: '/auth/register',
+      },
+      {
+        title: '权限管理功能',
+        link: '/auth/request-password',
+      },
+      {
+        title: '定时通断电',
+        link: '/auth/reset-password',
+      },
+    ],
+  },
+];
